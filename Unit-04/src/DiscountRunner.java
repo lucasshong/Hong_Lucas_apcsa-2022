880c191f99939046c6cf8f2953bb14d10da811d7
lucasshong <lucassd0921@gmail.com>
2022-01-25 01:02:43 -0800

    Unit 4 Assignments
    AddSub, Discount, and NumberVerify completed

---
+++ Unit-04/src/DiscountRunner.java
@@ -0,0 +1,26 @@
+//(c) A+ Computer Science
+//www.apluscompsci.com
+
+//Name - 
+//Date -
+//Class -
+//Lab  -
+
+import static java.lang.System.*;
+import java.util.Scanner;
+
+public class DiscountRunner
+{
+	public static void main( String args[] )
+	{
+		Scanner keyboard = new Scanner(System.in);
+		
+		out.print("Enter the original bill amount :: ");
+		double amt = keyboard.nextDouble();
+		
+		out.printf("Bill after discount :: ");
+		out.printf("%.2f\n", Discount.getDiscountedBill(amt));
+		
+
+	}
+}
